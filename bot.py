d852c4349359dd73f1085d296f88e33e487d74c7
Austin Isakson <austinjisakson@gmail.com>
2020-10-30 17:41:48 -0700

    Prints connected server names and their members to console (for debugging purposes).

--- bot.py
+++ bot.py
@@ -1,14 +1,19 @@
 # bot.py
-import os
 
 import keys
-import certifi
 import discord
 
 client = discord.Client()
 
+
 @client.event
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
+    print('Connected servers:')
+    for guild in client.guilds:
+        print('-', guild.name, '(id:', guild.id, ')')
+        print('-- Members:')
+        for member in guild.members:
+            print('---', member.name)
 
 client.run(keys.DISCORD_TOKEN)
